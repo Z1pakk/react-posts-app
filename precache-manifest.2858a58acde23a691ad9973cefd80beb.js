ba7a3a0db0924ca7f16facb7a311f86d1200cbf2
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-02-24 00:41:40 +0000

    Deploy Z1pakk/react-posts-app to github.com/Z1pakk/react-posts-app.git:gh-pages

---
+++ precache-manifest.2858a58acde23a691ad9973cefd80beb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "252605dc90a77b5b4683728bdd1c4808",
+    "url": "/react-posts-app/index.html"
+  },
+  {
+    "revision": "0eded93ed3ade1384982",
+    "url": "/react-posts-app/static/css/main.e2ee4236.chunk.css"
+  },
+  {
+    "revision": "c76894e236e348e15690",
+    "url": "/react-posts-app/static/js/2.ce18b7f9.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-posts-app/static/js/2.ce18b7f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0eded93ed3ade1384982",
+    "url": "/react-posts-app/static/js/main.5de759e6.chunk.js"
+  },
+  {
+    "revision": "27ebddb1febfd0b5d934",
+    "url": "/react-posts-app/static/js/runtime-main.d3e98b65.js"
+  }
+]);
